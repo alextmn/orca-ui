19eb9edfd4ca06a3fb1a1eabb2d3d67fa5d7f5f8
alex shpurov <alextmn1@gmail.com>
2025-05-09 09:39:26 -0400

    baseline project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule,
+    MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
